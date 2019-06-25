bc2355ed16160419ea7f4ebff5ed100e8dfb017e
Heli0os <44435709+Heli0os@users.noreply.github.com>
2019-06-25 13:36:31 +0200

    Update ViewPanel.java

--- src/View/ViewPanel.java
+++ src/View/ViewPanel.java
@@ -1,19 +1,26 @@
 package View;
 
 import Contract.IModel;
+import Model.Map;
+import Model.Point;
+import Model.Vector;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.Observable;
 import java.util.Observer;
 
 public class ViewPanel extends JPanel implements Observer
 {
     private final IModel model;
+    final BufferedImage logo = ImageIO.read(new File("C:\\Users\\Clément\\Pictures\\Cesi_Logo_INGENIEUR_RVB-360x213.png"));
     private ViewFrame viewFrame;
 
-    public ViewPanel(IModel model)
-    {
+    public ViewPanel(IModel model) throws IOException {
         this.model = model;
         if (this.model == null)
             System.err.println("model null in viewPanel builder");
@@ -52,45 +59,14 @@ public class ViewPanel extends JPanel implements Observer
         graphics2D.setColor(Color.BLACK);
         this.setFont(new Font("Dialog", Font.BOLD, 20));
         graphics2D.drawString("NOM DE LA MAP A INSERER", 375, 275);
-        graphics2D.setStroke(new BasicStroke(1));
-        graphics2D.drawOval(100,330,15,15);
-        graphics2D.drawOval(266,330,15,15);
-        graphics2D.drawOval(599,330,15,15);
-        graphics2D.drawOval(764,330,15,15);
-        graphics2D.drawOval(875,330,15,15);
-        graphics2D.drawOval(100,413,15,15);
-        graphics2D.drawOval(266,413,15,15);
-        graphics2D.drawOval(599,413,15,15);
-        graphics2D.drawOval(100,496,15,15);
-        graphics2D.drawOval(764,496,15,15);
-        graphics2D.drawOval(875,496,15,15);
-        graphics2D.drawOval(266,558,15,15);
-        graphics2D.drawOval(764,558,15,15);
-        graphics2D.drawOval(100,621,15,15);
-        graphics2D.drawOval(266 ,621,15,15);
-        graphics2D.drawOval(875,621,15,15);
-        graphics2D.setStroke(new BasicStroke(3));
-        graphics2D.drawLine(116,338, 264,338);
-        graphics2D.drawLine(282,338, 597,338);
-        graphics2D.drawLine(615,338, 762,338);
-        graphics2D.drawLine(780,338, 873,338);
-        graphics2D.drawLine(108,346, 108,411);
-        graphics2D.drawLine(274,346, 274,411);
-        graphics2D.drawLine(607,346, 607,411);
-        graphics2D.drawLine(108,429, 108,496);
-        graphics2D.drawLine(108,512, 108,621);
-        graphics2D.drawLine(116,421, 264,421);
-        graphics2D.drawLine(282,421, 597,421);
-        graphics2D.drawLine(772,346, 772,494);
-        graphics2D.drawLine(883,346, 883,494);
-        graphics2D.drawLine(116,504, 762,504);
-        graphics2D.drawLine(780,504, 873,504);
-        graphics2D.drawLine(116,629, 264,629);
-        graphics2D.drawLine(274,620, 274,574);
-        graphics2D.drawLine(282,566, 762,566);
-        graphics2D.drawLine(772,558, 772,512);
-        graphics2D.drawLine(282,629, 873,629);
-        graphics2D.drawLine(883,621, 883,512);
+        for (Point intersection : Map.getInstance().getIntersections()){
+            graphics2D.setStroke(new BasicStroke(1));
+            graphics2D.drawOval(intersection.getX(),intersection.getY(),15,15);
+        }
+        for (Vector connection : Map.getInstance().getConnections()){
+            graphics2D.setStroke(new BasicStroke(3));
+            graphics2D.drawLine(connection.getOrigin().getX()+8,connection.getOrigin().getY()+8,connection.getDestination().getX()+8,connection.getDestination().getY()+8);
+        }
     }
 
     public void paintVehiculeData(Graphics2D graphics2D)
@@ -100,9 +76,10 @@ public class ViewPanel extends JPanel implements Observer
         graphics2D.setColor(Color.BLACK);
         this.setFont(new Font("Dialog", Font.BOLD, 20));
         graphics2D.drawString("DONNEES DU VEHICULE", 175, 775);
-        graphics2D.drawString("POINT", 15, 850);
-        graphics2D.drawString("DISTANCE", 15, 900);
-        graphics2D.drawString("DISTANCE TOTALE", 15, 950);
+        graphics2D.drawString("Prochain point : " + this.model.getVehicles().get(0).getActualPosition().getDestination().getId(), 15, 850);
+        graphics2D.drawString("Distance parcourue sur la section : " + this.model.getVehicles().get(0).getDistanceOnSection() + " cm", 15, 880);
+        graphics2D.drawString("Distance totale parcourue : " + this.model.getVehicles().get(0).getTotalDistance() + " cm", 15, 910);
+        graphics2D.drawString("Vitesse du véhicule : " + this.model.getVehicles().get(0).getSpeed() + " m/s", 15, 940);
     }
 
     public void paintUserData(Graphics2D graphics2D)
@@ -112,7 +89,7 @@ public class ViewPanel extends JPanel implements Observer
         graphics2D.setColor(Color.BLACK);
         this.setFont(new Font("Dialog", Font.BOLD, 20));
         graphics2D.drawString("INFORMATIONS DU GROUPE", 225, 50);
-        graphics2D.drawString("GR6 - K2000 (Version 1.0)", 15, 100);
+        graphics2D.drawString("GRP6 - K2000 (Version 1.0)", 15, 100);
         graphics2D.drawString("AMARY Clément, CARRE Etienne, DE GROSSI Hugo, WEIMANN Théo", 15, 150);
     }
 
@@ -120,5 +97,6 @@ public class ViewPanel extends JPanel implements Observer
     {
         graphics2D.setColor(Color.WHITE);
         graphics2D.fillRect(700, 750, 300, 250);
+        graphics2D.drawImage(logo, 675, 750, null);
     }
 }
