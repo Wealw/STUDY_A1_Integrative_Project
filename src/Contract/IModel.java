f5773518e8e8f262b100115f57584b221e6fc5f4
Heli0os <44435709+Heli0os@users.noreply.github.com>
2019-06-25 13:38:30 +0200

    Update IModel.java

--- src/Contract/IModel.java
+++ src/Contract/IModel.java
@@ -1,4 +1,12 @@
 package Contract;
 
+import Model.Vehicle;
+
+import java.util.ArrayList;
+
 public interface IModel
-{}
+{
+    public ArrayList<Vehicle> getVehicles();
+
+    public void setVehicles(ArrayList<Vehicle> vehicles);
+}
