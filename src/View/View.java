a67574de12f670c67d690e139e35788d28a52989
Heli0os <44435709+Heli0os@users.noreply.github.com>
2019-06-25 13:37:16 +0200

    Update View.java

--- src/View/View.java
+++ src/View/View.java
@@ -4,26 +4,27 @@ import Contract.IController;
 import Contract.IModel;
 import Contract.IView;
 
+import java.io.IOException;
 import java.util.Observer;
 
 public class  View implements IView
 {
     private ViewFrame frame;
     private IController controller;
-    
-    public View(IModel model) {
+
+    public View(IModel model) throws IOException {
         if(model == null)System.err.println("model null in view");
         this.frame = new ViewFrame(model);
     }
-    
+
     public void close() {
         this.frame.dispose();
     }
-    
+
     public Observer getObserver() {
         return this.frame.getViewPanel();
     }
-    
+
     public void printMessage(final String message) {
         this.frame.printMessage(message);
     }
@@ -31,5 +32,4 @@ public class  View implements IView
     public void setController(IController controller) {
         this.controller = controller;
     }
-
 }
